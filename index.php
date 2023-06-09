569f13382c8f3fe26ace5b2e7234eef9dca53322
HilmiAnugrah <hilmianugrah2003@gmail.com>
2023-06-09 14:42:52 +0700

    merubah uri

--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <?php 
-session_start();
 $username = isset($_SESSION['username']) ? $_SESSION['username'] : "";
+$emailUser = isset($_SESSION['email']) ? $_SESSION['email'] : "";
 
 // Ambil URL saat ini
-$currentURL = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$currentURL = $_SERVER['REQUEST_URI'];
 
 // Tambahkan "/" di akhir URL untuk memastikan kecocokan dengan URL yang diharapkan
 $currentURL = rtrim($currentURL, "/");
 
 if ($currentURL == "haydeberita.my.id/index.php" || $currentURL == "haydeberita.my.id/") {
-    if (isset($_SESSION["login"])) {
-        $buttonText = "Dashboard";
-        $buttonLink = "dashboard.php?username=" .base64_encode($username)."&email=".base64_encode($emailUser);
-    } else {
-        $buttonText = "Masuk/Daftar";
-        $buttonLink = "login.php";
-    }
+  if (!empty($_SESSION["login"])) {
+      $buttonText = "Dashboard";
+      $buttonLink = "dashboard.php?username=" .base64_encode($username)."&email=".base64_encode($emailUser);
+  } else {
+      $buttonText = "Masuk/Daftar";
+      $buttonLink = "login.php";
+  }
 } else {
-    $buttonText = "Masuk/Daftar";
-    $buttonLink = "login.php";
+  $buttonText = "Masuk/Daftar";
+  $buttonLink = "login.php";
 }
+?>
+
 
 require('view/index.view.php'); 
