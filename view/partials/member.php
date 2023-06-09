f24431430543ff8b60bec324dbd89b4516734585
HilmiAnugrah <hilmianugrah2003@gmail.com>
2023-06-10 00:20:56 +0700

    menambakan query data pada section  User Member

--- view/partials/member.php
+++ view/partials/member.php
@@ -1,31 +1,34 @@
 <section class="member-link">
 <div class="container-section">
     <div class="title">
-    <h1>User Member</h1>
+    <h1>Member Area</h1>
     <p>Hayde Bismillah dulu!</p>
     </div>
     <div class="table-container">
         <table>
             <thead>
-                <th>Id</th>
+                <th>#</th>
                 <th>Image</th>
                 <th>Username</th>
                 <th>Email</th>
-                <th>action</th>
+                <th>Action</th>
             </thead>
             <tbody>
+            <?php $i = 1; ?>
+            <?php foreach ($users as $user) : ?>
                 <tr>
-                    <th scope="row">1</th>
+                    <th scope="row"><?= $i++; ?></th>
                     <td>
                         <img src="" alt="image" width="50">
                     </td>
-                    <td>HilmiAnugrah</td>
-                    <td>hilmianugrah@mail.com</td>
+                    <td><?= $user['username']; ?></td>
+                    <td><?= $user['email']; ?></td>
                     <td>
                         <a href="" class="badge text-bg-warning">ubah</a> |
                         <a href="" class="badge text-bg-danger">hapus</a>
                     </td>
                 </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
 
