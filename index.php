f25b60e31de1e1292d5c7fe3c3c4fc4f674128c9
HilmiAnugrah <hilmianugrah2003@gmail.com>
2023-06-09 14:51:37 +0700

    memperbaiki masalah button dashboard

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?php 
-$username = isset($_SESSION['username']) ? $_SESSION['username'] : "";
-$emailUser = isset($_SESSION['email']) ? $_SESSION['email'] : "";
-
-// Ambil URL saat ini
-$currentURL = $_SERVER['REQUEST_URI'];
-
-// Tambahkan "/" di akhir URL untuk memastikan kecocokan dengan URL yang diharapkan
-$currentURL = rtrim($currentURL, "/");
-
-if ($currentURL == "haydeberita.my.id/index.php" || $currentURL == "haydeberita.my.id/") {
-  if (!empty($_SESSION["login"])) {
-      $buttonText = "Dashboard";
-      $buttonLink = "dashboard.php?username=" .base64_encode($username)."&email=".base64_encode($emailUser);
-  } else {
-      $buttonText = "Masuk/Daftar";
-      $buttonLink = "login.php";
-  }
-} else {
-  $buttonText = "Masuk/Daftar";
-  $buttonLink = "login.php";
-}
-
+session_start();
 
 
 require('view/index.view.php'); 
--- view/partials/header.php
+++ view/partials/header.php
@@ -1,15 +1,16 @@
 <?php 
-
 $username = isset($_SESSION['username']) ? $_SESSION['username'] : "";
+$emailUser = isset($_SESSION['email']) ? $_SESSION['email'] : "";
 
 // Ambil URL saat ini
-$currentURL = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$currentURL = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
 
 // Tambahkan "/" di akhir URL untuk memastikan kecocokan dengan URL yang diharapkan
 $currentURL = rtrim($currentURL, "/");
 
 if ($currentURL == "haydeberita.my.id/index.php" || $currentURL == "haydeberita.my.id/") {
-  if (isset($_SESSION["login"])) {
+  if (!empty($_SESSION["login"])) {
       $buttonText = "Dashboard";
       $buttonLink = "dashboard.php?username=" .base64_encode($username)."&email=".base64_encode($emailUser);
   } else {
