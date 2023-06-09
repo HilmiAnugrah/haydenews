3f25116970f41532dac78ad6c623a45ea98fd3f0
HilmiAnugrah <hilmianugrah2003@gmail.com>
2023-06-09 14:36:57 +0700

    merubah link buton dashboard index

--- index.php
+++ index.php
@@ -3,15 +3,15 @@ session_start();
 $username = isset($_SESSION['username']) ? $_SESSION['username'] : "";
 
 // Ambil URL saat ini
-$currentURL = $_SERVER['REQUEST_URI'];
+$currentURL = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 // Tambahkan "/" di akhir URL untuk memastikan kecocokan dengan URL yang diharapkan
 $currentURL = rtrim($currentURL, "/");
 
-if ($currentURL == "/pw2023_223040175/tubes/index.php" || $currentURL == "/pw2023_223040175/tubes/") {
+if ($currentURL == "haydeberita.my.id/index.php" || $currentURL == "haydeberita.my.id/") {
     if (isset($_SESSION["login"])) {
         $buttonText = "Dashboard";
-        $buttonLink = "dashboard.php?username=" . urlencode($username);
+        $buttonLink = "dashboard.php?username=" .base64_encode($username)."&email=".base64_encode($emailUser);
     } else {
         $buttonText = "Masuk/Daftar";
         $buttonLink = "login.php";
