fc0634f4b6ec5f36b1e1421a5f8fd0369d7a4316
HilmiAnugrah <hilmianugrah2003@gmail.com>
2023-06-10 22:29:18 +0700

    memodifikasi session

--- delete.php
+++ delete.php
@@ -1,9 +1,9 @@
 <?php 
 session_start();
-// if(!isset($_SESSION["login"])){
-//     header("Location: login.php");
-//     exit;
-// }
+if(!isset($_SESSION["login"])){
+    header("Location: login.php");
+    exit;
+}
 $username = isset($_SESSION['username']) ? $_SESSION['username'] : "";
 $emailUser = isset($_SESSION['email']) ? $_SESSION['email'] : "";
 $encodeUser= base64_encode($username);
--- index.php
+++ index.php
@@ -1,3 +1,2 @@
 <?php 
-session_start();
 require('view/index.view.php'); 
