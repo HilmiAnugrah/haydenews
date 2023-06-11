229f28074513fbe158be0ae5a57f5036e67c098f
HilmiAnugrah <hilmianugrah2003@gmail.com>
2023-06-11 17:04:03 +0700

    kembali komit

--- view/partials/edit.php
+++ view/partials/edit.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 require "functions.php";
 if(isset($_GET['id'])){
@@ -20,59 +19,44 @@ if (isset($_POST['submit'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php require "view/partials/meta.php" ?>
-    <link rel="stylesheet" href="css/edit.css">
-    <!-- botstraplink -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css" />
-    <title>Hayde | Edit</title>
+    <link rel="stylesheet" href="css/member.css">
+    <title>edit</title>
 </head>
 <body>
-=======
->>>>>>> parent of 232afba (mengubah structur file)
 <section class="form-edit" id="form-edit">
 <div class="judul-container">
-    <h1>Ubah Data User</h1>
+    <h2>Ubah Data User</h2>
 </div>
 
 <form action="" method="post">
-<<<<<<< HEAD
-        <input type="hidden" name="id">
-           
-=======
+        <input type="hidden" name="id" value="<?= $userById['id']; ?>">
                 <div class="inputan-user">
->>>>>>> parent of 232afba (mengubah structur file)
                     <div class="username">
                         <label for="username">Username<span class="required"></span></label>
                         <?php if(isset($notSpasi)): ?>
                             <p style="color:red; font-style:italic;">Username tidak boleh mengandung spasi!!</p>
                         <?php endif; ?>
-<<<<<<< HEAD
-                        <input type="text" name="username" id="username" placeholder="Nama Anda" autofocus required ">
-=======
-                        <input type="text" name="username" id="username" placeholder="Nama Anda" autofocus required>
->>>>>>> parent of 232afba (mengubah structur file)
+                        <input type="text" name="username" id="username" placeholder="Nama Anda" autofocus required value="<?= $userById['username']; ?>">
                     </div>
                     <div class="email">
                         <label for="email">Email<span class="required"></span></label>
-                        <input type="email" name="email" id="email" placeholder="Email" required>
+                        <input type="email" name="email" id="email" placeholder="Email" requiredvalue="<?= $userById['email']; ?>">
                     </div>
        
                     <div class="password">
                         <label for="password">Password<span class="required"></span></label>
-                        <input type="password" name="password" id="password" placeholder="**********" required>
+                        <input type="password" name="password" id="password" placeholder="**********" requiredvalue="<?= $userById['password']; ?>">
                     </div>
                     <div class="konfirmasi-password">
                         <label for="konfirmasi-password">Konformasi Password<span class="required"></span></label>
-<<<<<<< HEAD
-                        <input type="password" name="konfirmasi-password" id="konfirmasi-password" placeholder="**********" required >
-=======
-                        <input type="password" name="konfirmasi-password" id="konfirmasi-password" placeholder="**********" required>
+                        <input type="password" name="konfirmasi-password" id="konfirmasi-password" placeholder="**********" required value="<?= $userById['konfirmasi-password']; ?>">
                     </div>
->>>>>>> parent of 232afba (mengubah structur file)
                     </div>
                     <div class="button-log-reg">
                     <button type="submit" name="ubah">Ubah Data</button>
                     </div>
                 </form>
 </section>
+</body>
+</html>
+
